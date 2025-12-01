c858009bc1c2360dc3a816d3dad4e6f0f23b838c
Dmitriy Arbuzov <dmitriyauiphone@gmail.com>
2025-12-01 09:23:30 +0500

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "MagicalRecordSPM",
+    platforms: [.iOS(.v17)],
+    products: [
+        .library(name: "MagicalRecordSPM", targets: ["MagicalRecordSPM"])
+    ],
+    targets: [
+        .target(
+            name: "MagicalRecordSPM",
+            path: "Sources/MagicalRecord"
+        )
+    ]
+)
